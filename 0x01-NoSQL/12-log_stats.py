d793a62f525f2c20edf71dd913118a70d368bcfb
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-04-29 19:16:19 +0100

    updating task .12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,16 +4,26 @@
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_collection = client.logs.nginx
+def get_log_stats(nginx_collection):
+    '''a function to get the so status'''
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
-methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print(f'{nginx_collection.count_documents({})} logs')
 
-print(f'{nginx_collection.count_documents({})} logs')
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({'method': method})
+        print(f'\tmethod {method}: {count}')
 
-print('Methods:')
-for method in methods:
-    count = nginx_collection.count_documents({'method': method})
-    print(f'\tmethod {method}: {count}')
+    print(f'{nginx_collection.count_documents({"path": "/status"})} status check')
 
-print(f'{nginx_collection.count_documents({"path": "/status"})} status check')
+
+def main():
+    '''main function'''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+
+    get_log_stats(client.logs.nginx)
+
+
+if '__name__' == '__main__':
+    main()
