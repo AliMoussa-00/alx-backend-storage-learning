1e01b208ef2cdb728b8fdab89ba71e504269d9ee
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-04-29 17:45:06 +0100

    fixing style for task .11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,6 +3,6 @@
 
 
 def schools_by_topic(mongo_collection, topic):
-    '''Python function that returns the list of school having a specific topic'''
+    '''function that returns the list of school having a specific topic'''
 
     return mongo_collection.find({'topics': {"$in": [topic]}})
